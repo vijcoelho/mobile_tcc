c6af0f3653adc7d59ab1db404131428410537e4a
Vitor Coelho <vijcoelho@gmail.com>
2024-04-21 12:57:04 -0300

    Update UserController
    Create post and getters methods.
    login, save and get current user

--- tcc/src/main/java/com/example/tcc/controller/UserController.java
+++ tcc/src/main/java/com/example/tcc/controller/UserController.java
@@ -2,8 +2,13 @@ package com.example.tcc.controller;
 
 import com.example.tcc.model.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.tcc.model.User;
 
 @RestController
 public class UserController {
@@ -12,5 +17,24 @@ public class UserController {
     private UserDao userDao;
 
     @PostMapping("user/save")
-    public 
+    public User save(@RequestBody User user) {
+        return userDao.save(user);
+    }
+
+    @PostMapping("user/login")
+    public ResponseEntity<String> authUser(@RequestBody User user) {
+        User authUser = userDao.authUser(user.getUserEmail(), user.getUserPassword());
+
+        if (authUser != null) {
+            String response = "Welcome, you're authenticated";
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Authentication failed");
+        }
+    }
+
+    @GetMapping("user/getCurrentUser")
+    public User getCurrentUser() {
+        return userDao.getCurrentUser();
+    }
 }
