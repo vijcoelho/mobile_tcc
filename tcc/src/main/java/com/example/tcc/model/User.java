1c6889e09a93e3e39eb9edab4a330c0cc722ae8b
Vitor Coelho <vijcoelho@gmail.com>
2024-04-21 11:25:19 -0300

    up

---
+++ tcc/src/main/java/com/example/tcc/model/User.java
@@ -0,0 +1,44 @@
+package com.example.tcc.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class User {
+
+    private int id;
+    private String userName;
+    private String userEmail;
+    private String userPassword;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+}
